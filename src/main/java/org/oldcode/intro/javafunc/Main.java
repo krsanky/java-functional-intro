65962bf47711f75ef2c2ec1439c40c0012a3d321
paul wisehart <paul@oldcode.org>
2018-01-19 08:39:09 -0500

    notice the Optional return

--- src/main/java/org/oldcode/intro/javafunc/Main.java
+++ src/main/java/org/oldcode/intro/javafunc/Main.java
@@ -1,17 +1,22 @@
 package org.oldcode.intro.javafunc;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("java functional ...");
-    }
-    static boolean isPrime(int num) {
         //Predicate<Integer> isDiv =  divisor -> num % divisor == 0;
-        Predicate<Integer> isDiv =  divisor -> num % divisor == 0;
-        return num > 1 &&
-                IntStream.range(2, num)
-                         .noneMatch(index -> num % index == 0);
+        
+        List<Integer> values = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        System.out.println(
+                values.stream()
+                        .filter(e -> e > 3)
+                        .filter(e -> e % 2 == 0)
+                        .map(e -> e * 2)
+                        .findFirst()
+//                        .get()
+        );
     }
 }
