97a9bce649e30abd5f383781f70b7c333044c9d7
paul wisehart <paul@oldcode.org>
2018-01-19 08:27:18 -0500

    something to see

--- src/main/java/org/oldcode/intro/javafunc/Main.java
+++ src/main/java/org/oldcode/intro/javafunc/Main.java
@@ -1,7 +1,17 @@
 package org.oldcode.intro.javafunc;
 
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("java functional ...");
+    }
+    static boolean isPrime(int num) {
+        //Predicate<Integer> isDiv =  divisor -> num % divisor == 0;
+        Predicate<Integer> isDiv =  divisor -> num % divisor == 0;
+        return num > 1 &&
+                IntStream.range(2, num)
+                         .noneMatch(index -> num % index == 0);
     }
 }
